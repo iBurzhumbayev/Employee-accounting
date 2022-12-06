ceabea081aab2cee88f355e3676b827d014512d0
Ilyas <kz.smile@bk.ru>
2022-12-06 15:54:30 +0600

    Практика свойств на проекте

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,27 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+	const data = [
+		{name: 'Ilyas', salary: 9000, increase: false},
+		{name: 'Askar', salary: 11000, increase: true},
+		{name: 'Aslan', salary: 8000, increase: false},
+		{name: 'Gleb', salary: 100000, increase: false},
+	]
+
+	return (
+		<div className="app">
+			<AppInfo />
+
+			<div className="search-panel">
+				<SearchPanel/>
+				<AppFilter/>
+			</div>
+			
+			<EmployeesList data={data}/>
+			<EmployeesAddForm/>
+		</div>
+	);
 }
 
 export default App;
